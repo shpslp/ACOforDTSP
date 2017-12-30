3b4309167732fa6f98f0a768c299bae237715d65
shpslp <urushidate.t.aa@t.titech.ac.jp>
2017-12-31 00:41:03 +0900

    ヒストグラムを出せるようにしました

--- PlotSkipSolution.m
+++ PlotSkipSolution.m
@@ -4,26 +4,26 @@ a1=subplot(2,2,1);
 subplot(2,2,1)
 PlotSolution(StaBestSol.Tour,model);
 title('All Tour Static')
-xlabel(a1,'y')
-ylabel(a1,'x')
+xlabel(a1,'x')
+ylabel(a1,'y')
 
 a2=subplot(2,2,2);
 PlotSolution(DynBestSol.Tour,model);
 title('All Tour Dynamic')
-xlabel(a2,'y')
-ylabel(a2,'x')
+xlabel(a2,'x')
+ylabel(a2,'y')
 
 a3=subplot(2,2,3);
 PlotSolution(RealTour.Sta,model);
 title(['Skip Tour Static -- Visited Number', num2str(numel(RealTour.Sta))])
-xlabel(a3,'y')
-ylabel(a3,'x')
+xlabel(a3,'x')
+ylabel(a3,'y')
 
 a4=subplot(2,2,4);
 PlotSolution(RealTour.Dyn,model);
 title(['Skip Tour Dynamic -- Visited Number: ', num2str(numel(RealTour.Dyn))])
-xlabel(a4,'y')
-ylabel(a4,'x')
+xlabel(a4,'x')
+ylabel(a4,'y')
 
 saveas(gcf,[pwd '/output/CompareSkipSolution.png'])
 end
--- main.m
+++ main.m
@@ -65,12 +65,55 @@ RealTour.Sta = SkipTour(StaBestSol,HPTour.Sta);
 RealTour.Dyn = SkipTour(DynBestSol,HPTour.Sta);
 PlotSkipSolution(StaBestSol,DynBestSol,RealTour,model)
 
-for i=1:500
+for i=1:1000
 RealTour.Sta = SkipTour(StaBestSol,HPTour.Sta);
-Cost.Sta = CostFunction(StaHP_Log,RealTour.Sta,model,par.aL,1);
+Cost.Sta = TourLength(RealTour.Sta,model);
 RealTour.Dyn = SkipTour(DynBestSol,HPTour.Sta);
-Cost.Dyn = CostFunction(DynHP_Log,RealTour.Dyn,model,par.aL,1);
-h(1,i)=Cost.Dyn/Cost.Sta;
+Cost.Dyn = TourLength(RealTour.Dyn,model);
+h1(1,i)=Cost.Sta;
+h2(1,i)=Cost.Dyn;
+n1(1,i)=numel(RealTour.Sta);
+n2(1,i)=numel(RealTour.Dyn);
 end
-figure
-hist=histogram(h)
+figure('Position',[180 0 480*2 420*2])
+f1=subplot(2,3,1);
+hist1=histogram(h1)
+title('Static Length Histogram')
+xlabel(f1,'Length')
+ylabel(f1,'freauency')
+
+f2=subplot(2,3,2);
+hist2=histogram(h2)
+title('Dynamic Length Histogram')
+xlabel(f2,'Length')
+ylabel(f2,'freauency')
+
+f3=subplot(2,3,3);
+hist1=histogram(h1)
+hold on
+hist2=histogram(h2)
+title('Compare Length Histogram')
+xlabel(f3,'Length')
+ylabel(f3,'freauency')
+legend('Static','Dynamic')
+
+f4=subplot(2,3,4);
+hist3=histogram(n1)
+title('Static Delivered Success Number')
+xlabel(f4,'Number')
+ylabel(f4,'freauency')
+
+f5=subplot(2,3,5);
+hist4=histogram(n2)
+title('Dynamic Delivered Success Number')
+xlabel(f5,'Number')
+ylabel(f5,'freauency')
+
+f6=subplot(2,3,6);
+hist3=histogram(n1)
+hold on
+hist4=histogram(n2)
+title('Compare Delivered Success Number')
+xlabel(f6,'Number')
+ylabel(f6,'freauency')
+legend('Static','Dynamic')
